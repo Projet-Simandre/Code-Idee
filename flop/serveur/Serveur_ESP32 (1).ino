8891352a123bceb1eab64cc7a93ed9b6c68731a4
enniriss <137659937+enniriss@users.noreply.github.com>
2023-12-06 14:50:40 +0100

    Add files via upload

---
+++ flop/serveur/Serveur_ESP32 (1).ino
@@ -0,0 +1,61 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFi.h>
+const char* ssid ="SimAndré";
+const char* password = "12345678";
+char Lclient;
+String n ="";
+WiFiServer server(80);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  // ----------------------------------------- Connexion WiFi ----------------------------------------------
+    Serial.println("\n[*] Creation de la zone WiFi");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+    Serial.print("[+] AP Created with IP Gateway ");
+    Serial.println(WiFi.softAPIP());
+    // -----------------------------------------------------------------------------------------------------
+     // initialize serial:
+
+  WiFi.begin(ssid, password);
+ 
+    server.begin();
+    
+    
+
+
+
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // listen for incoming clients
+  WiFiClient client = server.available();
+
+   if (client) {
+
+    
+     
+     while (client.connected()) {
+       
+    if (client.available()) {
+      Lclient = client.read();
+      n+= Lclient;
+      
+    }
+
+  } 
+  Serial.println(n);
+   
+
+  
+}
+n=""; 
+
+
+
+}
